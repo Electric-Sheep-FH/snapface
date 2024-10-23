3a301fbc16ea479ede95a8a73f1191b28113b3c2
Fabien Hasselmann <fabien.hasselmann@gmail.com>
2024-10-23 11:05:06 +0200

    ajout guards et restructuration des fichiers et route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
+import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    HeaderComponent,
     RouterOutlet,
     AsyncPipe,
+    CoreModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,13 @@
-import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { htttpInterceptorProviders } from './interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    htttpInterceptorProviders,
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    { provide: LOCALE_ID, useValue: 'fr-FR' }
-
-  ]
+  ],
 };
